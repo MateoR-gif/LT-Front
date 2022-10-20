631079a14e59c1059ff9a52f5fe9c521a004b54f
Simon <londonosimon67873@gmail.com>
2022-10-19 21:57:29 -0500

    Estructuracion del componente register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Login from './components/login/Login';
+import Login from './pages/login/Login';
 
 function App() {
   return (
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Register() {
+  const [error, setError] = useState('Todo en orden, putos!')
+  const handleChange = ({ target: { name, value } }) => {
+    setUser ({ ...user, [name]: value})
+  }
   return (
-    <div>Register</div>
+    <div className='register__container'>
+      <div className='email__input__container'>
+        <input
+          className='email__input'
+          type='email'
+          name='email'
+          onChange={handleChange}
+          placeholder='Correo'
+        />
+      </div>
+      <div className='password__input__container'>F
+        <input
+          className='password__input'
+          type='password'
+          name='password'
+          onChange={handleChange}
+          placeholder='Contraseña'
+        />
+
+      </div>
+      <div className='register__buton__container'>
+        <button className='register__button'> Registrarse </button>
+      </div>
+      <div className='error__msg__container'>
+        <p className='error__msg'>{error}</p>
+      </div>
+    </div>
   )
 }
