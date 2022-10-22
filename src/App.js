7541abe60af9362bac473a51a2d264fcb465bf70
MateoR-gif <mateorendon008@gmail.com>
2022-10-22 17:00:27 -0500

    Creación e Integración de Ruta Protegida | Carpeta Utils

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register'
-import Chat from './pages/chat/Chat'
+import Login from './pages/login/Login.js';
+import Register from './pages/register/Register.js'
+import Chat from './pages/chat/Chat.js'
+import ProtectedRoute from './components/ProtectedRoute.js';
+import NotFound from './pages/404/404'
+import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Chat/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
+        <Route path='/' element={
+          <ProtectedRoute>
+            <Chat />
+          </ProtectedRoute>
+        } />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
